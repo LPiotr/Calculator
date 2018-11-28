9ff6cbec14ada3a0b91f538f56e10c3cbc5380ab
LPiotr <piotrl1@live.com>
2018-11-28 16:04:46 +0100

    Initial commit

---
+++ Calc/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CalcTests/CalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(10, 15);
+
+            Assert.AreEqual<double>(25, result);
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
